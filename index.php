af280a43293b08e93a4edb27b9eb4483af7c5648
Zhukov_Vadim <zhukov@pizzafab.ru>
2017-10-10 19:10:49 +0300

    Alpha 0.1
    + Добавлены формы для всех филиалов
    + 2 режима команд для ПМ
    + Добавлена кнопка сброса режима
    + Автоматическое генерирование название текущего лога
    + Добавлено описание в фале README.md
    * Доработан обработчик форм
    + Автоматическое удаление ini файлов через 3 секунды
    * Данные из форм хранятся в ini файлах
    - Удалены каталоги actions и  json
    - Удален обработчик pm1.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+# Режим дебага
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -18,7 +24,7 @@
 		</thead>
 		<tbody>
 			<tr align="center">
-				<form method="post" action="../actions/pm1.php">
+				<form method="post" action="">
 					<input type="hidden" name="point" value="1">
 					<td>1</td>
 					<td>Бескудниково</td>
@@ -29,10 +35,302 @@
 							<option value="2">Просмотреть лог</option>
 						</select>
 					</td>
-					<td><input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить"></td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="2">
+					<td>2</td>
+					<td>Митино</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="6">
+					<td>6</td>
+					<td>Свиблово</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="7">
+					<td>7</td>
+					<td>Строгино</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="8">
+					<td>8</td>
+					<td>Кантемировская</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="9">
+					<td>9</td>
+					<td>Домодедовская</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="10">
+					<td>10</td>
+					<td>Дубровка</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="11">
+					<td>11</td>
+					<td>Пионерская</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="13">
+					<td>13</td>
+					<td>Бибирево</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="15">
+					<td>15</td>
+					<td>Перово</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="16">
+					<td>16</td>
+					<td>Чертаново</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="17">
+					<td>17</td>
+					<td>Бутово</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="18">
+					<td>18</td>
+					<td>Академическая</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="19">
+					<td>19</td>
+					<td>Орджоникидзе</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="22">
+					<td>22</td>
+					<td>Ясенево</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
+				</form>
+			</tr>
+			<tr align="center">
+				<form method="post" action="">
+					<input type="hidden" name="point" value="24">
+					<td>24</td>
+					<td>Водный Стадион</td>
+					<td>
+						<select name="mode" class="form-control">
+							<option disabled selected>Выберите режим</option>
+							<option value="1">Перезагрузить ПМ</option>
+							<option value="2">Просмотреть лог</option>
+						</select>
+					</td>
+					<td>
+						<input type="submit" name="send" class="btn btn-small btn-success" value="Выполнить">
+						<input type="reset" name="reset" class="btn btn-small btn-inverse" value="Сброс">
+					</td>
 				</form>
 			</tr>
 		</tbody>
 	</table>
+<?php
+
+if(isset($_POST['send'])) {
+
+	$file = fopen($_SERVER['DOCUMENT_ROOT'].'/pm'.$_POST['point'].'.ini', 'w');
+
+	fwrite($file, "[pm1]\n point = ".$_POST['point']."\n mode = ".$_POST['mode']."\n logdate = ".date('Ymd').".log\n ");
+
+	fclose($file);
+
+	sleep(3);
+
+	unlink($_SERVER['DOCUMENT_ROOT'].'/pm'.$_POST['point'].'.ini');
+
+}
+
+
+?>
+<script type="text/javascript" src="../style/js/bootstrap.min.js"></script>
 </body>
 </html>
